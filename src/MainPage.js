552097128fa7e9a29b7029773563154e1bc48453
Richard Carmen Scott <RichardCarmenScott@gmail.com>
2020-09-01 13:14:40 -0400

    added notes not folders, and css

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import './MainPage.css';
+/* import { Link } from 'react-router-dom'; */
 /* import { Route } from 'react-router-dom';
 import NotePage from './NotePage';
 import FolderPage from './FolderPage'; */
 
 export default function MainPage(props) {
-  const notes = props.state.folders.map((folder, idx) => {
-    return (<li key={idx}>{folder.name}</li>)
+  const notes = props.state.notes.map((note, idx) => {
+    return (
+    <li key={idx}>
+      <div className='notes-main'>
+      <h3>{note.name}</h3>
+      <p>{note.content}</p>
+      </div>
+    </li>
+    
+    )
   })
   return (
     <div>
       MainPage Showing
-      <Link to={'/NotePage'}>NotePage</Link>
       {/* <FolderList />
       <NoteList /> */}
       <ul>{notes}</ul>
